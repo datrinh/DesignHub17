aa4c279b544563e5f8c2cd0b31af3a100237b8a1
Duc Anh Trinh <ducanh.trinh@hotmail.de>
2017-09-27 13:29:25 +0200

    github update

--- src/app/layout/my-sidebar/my-sidebar.component.ts
+++ src/app/layout/my-sidebar/my-sidebar.component.ts
@@ -1,77 +1,77 @@
-import { Component, OnInit } from '@angular/core';
-import { MdSnackBar } from '@angular/material';
-import { AudioCommentService } from '../../shared/audio-comment/audio-comment.service';
-import { VideoService } from '../../video/video.service';
-import { BookmarkService } from '../../shared/bookmark/bookmark.service';
+// import { Component, OnInit } from '@angular/core';
+// import { MdSnackBar } from '@angular/material';
+// import { AudioCommentService } from '../../shared/audio-comment/audio-comment.service';
+// import { VideoService } from '../../video/video.service';
+// import { BookmarkService } from '../../shared/bookmark/bookmark.service';
 
-@Component({
-  selector: 'dh-my-sidebar',
-  templateUrl: './my-sidebar.component.html',
-  styleUrls: ['./my-sidebar.component.scss']
-})
-export class MySidebarComponent implements OnInit {
-  isBookmarksVisible = false;
-  isCommentsVisible = false;
-  isVibrationsVisible = false;
-  isSettingsVisible = false;
+// @Component({
+//   selector: 'dh-my-sidebar',
+//   templateUrl: './my-sidebar.component.html',
+//   styleUrls: ['./my-sidebar.component.scss']
+// })
+// export class MySidebarComponent implements OnInit {
+//   isBookmarksVisible = false;
+//   isCommentsVisible = false;
+//   isVibrationsVisible = false;
+//   isSettingsVisible = false;
 
-  constructor(
-    public bookmarks: BookmarkService,
-    private video: VideoService,
-    public audio: AudioCommentService,
-    private snackbar: MdSnackBar
-  ) { }
+//   constructor(
+//     public bookmarks: BookmarkService,
+//     private video: VideoService,
+//     public audio: AudioCommentService,
+//     private snackbar: MdSnackBar
+//   ) { }
 
-  ngOnInit() {
-  }
+//   ngOnInit() {
+//   }
 
-  jumpToTimestamp(timestamp: number) {
-    this.video.player.currentTime = timestamp;
-    this.video.player.pause();
-  }
+//   jumpToTimestamp(timestamp: number) {
+//     this.video.player.currentTime = timestamp;
+//     this.video.player.pause();
+//   }
 
-  deleteBookmark(id: number) {
-    this.bookmarks.deleteBookmark(id);
-    this.snackbar.open('Lesezeichen wurde gelöscht.', null, { duration: 2000 });
-  }
+//   deleteBookmark(id: number) {
+//     this.bookmarks.deleteBookmark(id);
+//     this.snackbar.open('Lesezeichen wurde gelöscht.', null, { duration: 2000 });
+//   }
 
-  deleteRecord(id: number) {
-    this.audio.deleteRecord(id);
-    this.snackbar.open('Kommentar wurde gelöscht.', null, { duration: 2000 });
-  }
+//   deleteRecord(id: number) {
+//     this.audio.deleteRecord(id);
+//     this.snackbar.open('Kommentar wurde gelöscht.', null, { duration: 2000 });
+//   }
 
-  sidebarOnChange(value: any) {
-    switch (value) {
-      case 'bookmark':
-        this.isCommentsVisible = false;
-        this.isVibrationsVisible = false;
-        this.isSettingsVisible = false;
-        this.isBookmarksVisible = !this.isBookmarksVisible;
-        break;
-      case 'comment':
-        this.isBookmarksVisible = false;
-        this.isVibrationsVisible = false;
-        this.isSettingsVisible = false;
-        this.isCommentsVisible = !this.isCommentsVisible;
-        break;
-      case 'vibration':
-        this.isBookmarksVisible = false;
-        this.isCommentsVisible = false;
-        this.isSettingsVisible = false;
-        this.isVibrationsVisible = !this.isVibrationsVisible;
-        break;
-      case 'settings':
-        this.isBookmarksVisible = false;
-        this.isCommentsVisible = false;
-        this.isVibrationsVisible = false;
-        this.isSettingsVisible = !this.isSettingsVisible;
-        break;
-      default:
-        this.isBookmarksVisible = false;
-        this.isCommentsVisible = false;
-        this.isVibrationsVisible = false;
-        this.isSettingsVisible = false;
-        break;
-    }
-  }
-}
+//   sidebarOnChange(value: any) {
+//     switch (value) {
+//       case 'bookmark':
+//         this.isCommentsVisible = false;
+//         this.isVibrationsVisible = false;
+//         this.isSettingsVisible = false;
+//         this.isBookmarksVisible = !this.isBookmarksVisible;
+//         break;
+//       case 'comment':
+//         this.isBookmarksVisible = false;
+//         this.isVibrationsVisible = false;
+//         this.isSettingsVisible = false;
+//         this.isCommentsVisible = !this.isCommentsVisible;
+//         break;
+//       case 'vibration':
+//         this.isBookmarksVisible = false;
+//         this.isCommentsVisible = false;
+//         this.isSettingsVisible = false;
+//         this.isVibrationsVisible = !this.isVibrationsVisible;
+//         break;
+//       case 'settings':
+//         this.isBookmarksVisible = false;
+//         this.isCommentsVisible = false;
+//         this.isVibrationsVisible = false;
+//         this.isSettingsVisible = !this.isSettingsVisible;
+//         break;
+//       default:
+//         this.isBookmarksVisible = false;
+//         this.isCommentsVisible = false;
+//         this.isVibrationsVisible = false;
+//         this.isSettingsVisible = false;
+//         break;
+//     }
+//   }
+// }
